a7e156b8b62fe21324d8e04da434c5d5b6180b10
Nancy Jain <nancyjain0631@gmail.com>
2020-07-12 22:27:16 +0530

    last part-2

--- js/script.js
+++ js/script.js
@@ -89,7 +89,10 @@ lightbox.addEventListener("click", function (event) {
 //Aside Navbar
 const nav = document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length;
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section");
+totalSection = allSection.length;
+
 for (let i = 0; i < totalNavList; i++) {
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function () {
@@ -97,5 +100,16 @@ for (let i = 0; i < totalNavList; i++) {
             navList[j].querySelector("a").classList.remove("active");
         }
         this.classList.add(".active");
+        showSection(this);
     })
+}
+
+function showSection(element) {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+
+
 }
